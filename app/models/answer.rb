36cfbf8dc27e2ce10135a42ff6c8de805c6dac84
larkk <lark.kudr@gmail.com>
2015-06-01 22:10:04 +0300

    no other def

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,14 +1,10 @@
 class Answer < ActiveRecord::Base
 	belongs_to :question
 
-    after_save :touch_answer
-
 	validates_presence_of :text
 	validates_length_of :text, :minimum => 10, :maximum => 1000
 
-	def touch_answer
-      answer.touch
-    end
+	
 
 
 end
