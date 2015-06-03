3ba4352119acd8d6d32e97caa49825e450de2052
larkk <lark.kudr@gmail.com>
2015-06-03 20:24:41 +0300

    l3done

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,8 +1,7 @@
 class Answer < ActiveRecord::Base
 	belongs_to :question
 
-	 validates :text, presence: true
-	validates :text, length: { minimum: 10, maximum: 1000 }
+	validates :text, presence: true, length: { minimum: 10, maximum: 1000 }
 
 
 end
